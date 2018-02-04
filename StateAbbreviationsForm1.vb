00134acb877c72d718c06b2e6f9a6dbcc4542fe0
ocdleyendo <ocdleyendo@gmail.com>
2018-02-04 13:48:11 -0500

    Add files via upload

---
+++ StateAbbreviationsForm1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        lblState.Text = "VA"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        lblState.Text = "NC"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        lblState.Text = "SC"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        lblState.Text = "GA"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        lblState.Text = "AL"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        lblState.Text = "FL"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Me.Close()
+    End Sub
+End Class
